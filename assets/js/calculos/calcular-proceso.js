284446050c0fc2988e2ad14905fe96d31a38829d
sebastianpitre <110608164+sebastianpitre@users.noreply.github.com>
2023-10-24 17:56:09 -0500

    CALCULO TOTAL

--- assets/js/calculos/calcular-proceso.js
+++ assets/js/calculos/calcular-proceso.js
@@ -1,18 +1,36 @@
-let parametroErosion = document.getElementById("parametroErosion").value;
-let movimientoMasa = document.getElementById("movimientoMasa").value;
-let drenajeNatural = document.getElementById("drenajeNatural").value;
-let inundaciones = document.getElementById("inundaciones").value;
-let encharcamientos = document.getElementById("encharcamientos").value;
-let profundidad = document.getElementById("profundidad").value;
-let textura = document.getElementById("textura").value;
-let pedregosidad = document.getElementById("pedregosidad").value;
-let afloramiento = document.getElementById("afloramiento").value;
-let contenidoSales = document.getElementById("contenidoSales").value;
-let contenidoSodio = document.getElementById("contenidoSodio").value;
-let sales_Sodio = document.getElementById("sales_Sodio").value;
-let saturacionAluminio = document.getElementById("saturacionAluminio").value;
-let distribucionLluvia = document.getElementById("distribucionLluvia").value;
-let pisosTermicos = document.getElementById("pisosTermicos").value;
-let condicionHumedad = document.getElementById("condicionHumedad").value;
-let temperatura = document.getElementById("temperatura").value;
+const parametroErosion = document.getElementById("parametroErosion").value;
+const movimientoMasa = document.getElementById("movimientoMasa").value;
+const drenajeNatural = document.getElementById("drenajeNatural").value;
+const inundaciones = document.getElementById("inundaciones").value;
+const encharcamientos = document.getElementById("encharcamientos").value;
+const profundidad = document.getElementById("profundidad").value;
+const textura = document.getElementById("textura").value;
+const pedregosidad = document.getElementById("pedregosidad").value;
+const afloramiento = document.getElementById("afloramiento").value;
+const contenidoSales = document.getElementById("contenidoSales").value;
+const contenidoSodio = document.getElementById("contenidoSodio").value;
+const sales_Sodio = document.getElementById("sales_Sodio").value;
+const saturacionAluminio = document.getElementById("saturacionAluminio").value;
+const distribucionLluvia = document.getElementById("distribucionLluvia").value;
+const pisosTermicos = document.getElementById("pisosTermicos").value;
+const condicionHumedad = document.getElementById("condicionHumedad").value;
+const temperatura = document.getElementById("temperatura").value;
 
+// Agrega más campos aquí para otros datos
+parametroErosion: document.getElementById("parametroErosion").value,
+movimientoMasa: document.getElementById("movimientoMasa").value,
+drenajeNatural: document.getElementById("drenajeNatural").value,
+inundaciones: document.getElementById("inundaciones").value,
+encharcamientos: document.getElementById("encharcamientos").value,
+profundidad: document.getElementById("profundidad").value,
+textura: document.getElementById("textura").value,
+pedregosidad: document.getElementById("pedregosidad").value,
+afloramiento: document.getElementById("afloramiento").value,
+contenidoSales: document.getElementById("contenidoSales").value,
+contenidoSodio: document.getElementById("contenidoSodio").value,
+sales_Sodio: document.getElementById("sales_Sodio").value,
+saturacionAluminio: document.getElementById("saturacionAluminio").value,
+distribucionLluvia: document.getElementById("distribucionLluvia").value,
+pisosTermicos: document.getElementById("pisosTermicos").value,
+condicionHumedad: document.getElementById("condicionHumedad").value,
+temperatura: document.getElementById("temperatura").value,
