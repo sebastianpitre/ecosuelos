cffa0ccaef886add504b30e867bdf88f9ea4ca17
sebastianpitre <sebastianandrespitrediaz@gmail.com>
2023-11-08 11:50:54 -0500

    NAV Y AUDIOS

--- assets/js/instrucciones/instrucciones.js
+++ assets/js/instrucciones/instrucciones.js
@@ -3,16 +3,16 @@ function intro_calcular() {
     intro.setOptions({
             steps: [
                 { element: document.getElementById(''),
-                intro: 'Hola, soy tu eco-asistente de voz <audio autoplay class="w-100 mt-3" controls><source src="../assets/mp3/intro1.mp3" type="audio/mpeg"></audio>presiona Next para avanzar',
+                intro: 'Hola, soy tu asistente de voz, y te voy a explicar paso a paso como funciona ecosuelos<audio autoplay class="w-100 mt-3" controls><source src="../assets/mp3/calcular1.mp3" type="audio/mpeg"></audio>presiona siguiente para avanzar',
                 step: 0,
                  position: 'right',
                  x: 100, // Coordenada X
                  y: 0, // Coordenada Y
                  },
                 { element: document.getElementById('intro1'), 
-                intro: 'Este es el formulario que debes diligenciar con la información de tu suelo<audio autoplay class="w-100 mt-3" controls><source src="../assets/mp3/example.mp3" type="audio/mpeg"></audio>presiona Next para avanzar', step: 1 },
+                intro: 'Este es el formulario que debes diligenciar con las características de tus suelos<audio autoplay class="w-100 mt-3" controls><source src="../assets/mp3/calcular2.mp3" type="audio/mpeg"></audio>presiona siguiente para ver más detalles', step: 1 },
                 { element: document.getElementById('intro2'),
-                 intro: 'Estos campos con un color oscuro se llenan automáticamente luego de hacer el siguiente paso <audio autoplay class="w-100 mt-3" controls><source src="../assets/mp3/example.mp3" type="audio/mpeg"></audio>presiona Next para avanzar', step: 2 },
+                 intro: 'En el formulario nos encontramos con las diferentes características del suelo<audio autoplay class="w-100 mt-3" controls><source src="../assets/mp3/calcular3.mp3" type="audio/mpeg"></audio>presiona siguiente para avanzar', step: 2 },
                 { element: document.getElementById('intro3'),
                  intro: 'presiona este icono para abrir una ventana emergente donde completaras los campos con su información correspondiente, luego haz clic en el botón para calcular la información y haz clic en la "X" para volver <audio autoplay class="w-100 mt-3" controls><source src="../assets/mp3/example.mp3" type="audio/mpeg"></audio>presiona Next para avanzar', step: 3 },
                  
